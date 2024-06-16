909c17ce84230d1630b291997e7d125a50771f06
Barasa <timbaros87@gmail.com>
2024-06-16 17:13:01 +0300

    Add handleDelete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onUpdateItem, onDeleteItem }) {
 
   function handleAddToCartClick() {
     fetch(`http://localhost:4000/items/${item.id}`, {
@@ -13,8 +13,18 @@ function Item({ item }) {
       }),
     })
       .then((r) => r.json())
-      .then((updatedItem) => console.log(updatedItem));
-  }
+      .then((updatedItem) => onUpdateItem(updatedItem));
+
+      }
+
+      function handleDeleteClick() {
+        fetch(`http://localhost:4000/items/${item.id}`, {
+          method: "DELETE",
+        })
+          .then((r) => r.json())
+          .then(() => onDeleteItem(item));
+      }
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
@@ -24,7 +34,7 @@ function Item({ item }) {
       onClick={handleAddToCartClick} >
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      <button className="remove" onClick={handleDeleteClick}>Delete</button>
     </li>
   );
 }
