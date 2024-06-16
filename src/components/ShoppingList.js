09510b852be3611a26d52d45d9961b98a5213cdf
Barasa <timbaros87@gmail.com>
2024-06-16 14:13:18 +0300

    Add handleAddItem function

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -11,7 +11,11 @@ function ShoppingList() {
     fetch("http://localhost:4000/items")
     .then((r) => r.json())
     .then((items) => setItems(items));
-  })
+  }, []);
+
+  function handleAddItem(newItem) {
+    console.log("In shoppingList:", newItem);
+  }
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -25,7 +29,7 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
